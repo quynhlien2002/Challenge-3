d33863edfec7e79896673dfe78d2598c9e675e43
Daisy Lien <daisylien1211@gmail.com>
2022-07-20 16:25:32 -0700

    change

--- script.js
+++ script.js
@@ -13,37 +13,55 @@ function writePassword() {
 function generatePassword(){
   console.log("Generate Password");
 
-  var choices;
+  var choices = "";
   var answer;
+
   
   var userChoice = window.prompt("How long is your password?");
+  console.log(userChoice);
   if (!userChoice) {
-    return;
+    return "";
   }
+
 
   var confirmLowerCase = window.confirm ("Do you want to include lowercase?");
-  if (confirmLowerCase) {
-    choices = choices + "abcdefghi"
-    var confirmUpperCase = window.confirm ("Do you want uppercase?");
-  }
-  if (confirmUpperCase) {
-    choices = choices + "ABCDE"
-    var confirmNumeric = window.confirm ("Do you want to contain number?");
-  }
-  if (confirmNumeric) {
-    choices = choices + "0123456789"
-    var confirmSpecialCharacter = window.confirm ("Do you want special characters?");
-  }
-  if (confirmSpecialCharacter) {
-    choices = choices + "!@#$"}
-
-for (var i = 0; i < parseInt (userChoice); i++){
-  var index = Math.floor (Math.random() * choices.length)
-  var addToAnswer = choices [index]
-  console.log(addToAnswer)
-}
   
-return "This will be your password";
+  if(confirmLowerCase) {
+    choices += "abcdefghijklmnopqrstuvwxyz";
+  } 
+  var confirmUpperCase = window.confirm ("Do you want uppercase?");
+
+  if (confirmUpperCase) {
+    choices +=  "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  } 
+  var confirmNumeric = window.confirm ("Do you want to contain number?");
+
+  if (confirmNumeric) {
+    choices += "0123456789";
+  }
+  
+  var confirmSpecialCharacter = window.confirm ("Do you want special characters?");
+
+  if (confirmSpecialCharacter) {
+    choices += "!@#$`~%^&*()_-+=|]{['";
+  }
+
+  if (choices == ""){
+      alert("Please select at least 1 option");
+      return "";
+  }
+  
+    var addToAnswer = "";
+    console.log(choices);
+
+    for (var i = 0; i < parseInt (userChoice); i++){
+        var index = Math.floor (Math.random() * choices.length)
+        addToAnswer += choices [index]
+        console.log(addToAnswer)
+    }
+  
+return addToAnswer;
+
 }
 
 // Add event listener to generate button
